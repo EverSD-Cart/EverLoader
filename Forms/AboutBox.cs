0afe0c9058c80b2d00f16be1b0935aeab162c265
EverLoader-dev <everloaderdev@gmail.com>
2022-02-03 22:01:56 +0100

    About box info

---
+++ Forms/AboutBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace EverLoader.Forms
+{
+    partial class AboutBox : Form
+    {
+        public AboutBox()
+        {
+            InitializeComponent();
+
+            lblInfo.Text = lblInfo.Text
+                .Replace("{ProductVersion}", Application.ProductVersion);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //open EverSD website
+            Process.Start(new ProcessStartInfo("https://eversd.com") { UseShellExecute = true });
+        }
+    }
+}
